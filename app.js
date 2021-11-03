e1c10bc4418c3d21bce725c2bb6e0f79adc24b54
Hkdp_Omtri <s544929@nwmissouri.edu>
2021-11-03 13:47:32 -0500

    files

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var textbookRouter = require('./routes/textbook')
+var textbookRouter = require('./routes/textbook');
 
 var app = express();
 
